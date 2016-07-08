a49d478f340d25ee74b93eec97b14a58dd1af804
Wes Hediger <wes.hediger@gmail.com>
2016-07-08 14:23:46 -0600

    add echo program

---
+++ src/github.com/whediger/hello/echo2.go
@@ -0,0 +1,16 @@
+//echo2 prints its commandline arguments
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
