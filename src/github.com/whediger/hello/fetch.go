8978c8e8eb4431e187f2b436ebbf691f49478478
Wes Hediger <wes.hediger@gmail.com>
2016-07-08 21:28:46 -0600

    add check for http:// and automatically append if missing

--- src/github.com/whediger/hello/fetch.go
+++ src/github.com/whediger/hello/fetch.go
@@ -7,12 +7,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	buf := make([]byte, 8)
 
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
