7557fd0dfb5940d74e084043bb083eea6ae8cad8
Wes Hediger <wes.hediger@gmail.com>
2016-07-08 21:16:22 -0600

    change io.Copy to operate with a buffer

--- src/github.com/whediger/hello/fetch.go
+++ src/github.com/whediger/hello/fetch.go
@@ -3,24 +3,24 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	buf := make([]byte, 8)
+
 	for _, url := range os.Args[1:] {
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-			os.Exit(1)
+		if _, err := io.CopyBuffer(os.Stdout, resp.Body, buf); err != nil {
+			log.Fatal(err)
 		}
-		fmt.Printf("%s", b)
+		fmt.Printf("%s", os.Stdout)
 	}
 }
